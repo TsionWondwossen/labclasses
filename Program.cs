475bd35fc766882876ff7b603eed95450b7c45c6
TsionWondwossen <tsionwondwossen8@gmail.com>
2022-09-15 20:58:08 +0300

    name,email,address

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_class_1
+{
+    class Student
+    {
+        private int _id;
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+        public String name { get; set; }
+        public String adress { get; set; }
+        public String email { get; set; }
+
+    }
+    class Program : Student
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Id = 10;
+            Console.WriteLine(student.Id);
+        }
+    }
+}
